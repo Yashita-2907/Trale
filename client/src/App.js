b549a0ddecf282777371eaae631187ec37850f01
paarthchauhan <114211182+paarthchauhan@users.noreply.github.com>
2025-04-17 11:00:14 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { Home, Login, Profile, Register, ResetPassword } from "./pages";
 import { useSelector } from "react-redux";
-
+//a
 function Layout() {
   const { user } = useSelector((state) => state.user);
   const location = useLocation();
